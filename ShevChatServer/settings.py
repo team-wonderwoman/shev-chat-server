9f838cb65930f6d11f195c39675249809a920189
kwonheejeong <gmlwjd9405@naver.com>
2018-01-29 15:49:54 +0900

    Set basic routing and channels setting

--- ShevChatServer/settings.py
+++ ShevChatServer/settings.py
@@ -27,6 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+
+# Channel layer definitions
+# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
+CHANNEL_LAYERS = {
+    "default": {
+        # This example app uses the Redis channel layer implementation asgi_redis
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(redis_host, 6379)],
+        },
+       "ROUTING": "ShevChatServer.routing.channel_routing", # We will create it in a moment
+    },
+}
 
 # Application definition
 
@@ -37,6 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
+    'channels'
 ]
 
 MIDDLEWARE = [
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def index(request):
+    """
+    Root page view. This is essentially a SPA, if you ignore the login and admin parts.
+    :param request:
+    :return: render that in the index template
+    """
+    return render(request, "index.html")
+
