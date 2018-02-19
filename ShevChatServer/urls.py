ad02f4769a6c62ddf4468d42c73ace6a2ab0a3cc
kwonheejeong <gmlwjd9405@naver.com>
2018-02-19 16:42:19 +0900

    Update requirements2.txt and Modify db setting

--- ShevChatServer/urls.py
+++ ShevChatServer/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from chat.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', index, name='index'),  ## for test
-
     url(r'^admin/', admin.site.urls),
     url(r'^api/group/', include('chat.urls', namespace='groups-api'))
 ]
