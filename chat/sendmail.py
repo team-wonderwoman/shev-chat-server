91c25e8f54e0d8c61a80fcd99401da48574e57f5
kwonheejeong <gmlwjd9405@naver.com>
2018-02-23 09:39:06 +0900

    Delete email password

--- chat/sendmail.py
+++ chat/sendmail.py
@@ -15,20 +15,6 @@ from email.mime.application import MIMEApplication
 from common.const import const_value
 # from AuthSer.redis import redis_set
 
-# def create_verify_token(groupId,query):
-#     payload = {
-#         'group_id' : groupId,
-#         'user_email': query.user_email,
-#         'user_name': query.user_name,
-#         'datetime': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-#     }
-#
-#     verify_token = jwt.encode(payload, "SECRET_KEY", algorithm='HS256')
-#
-#     print("verify_token")
-#     print(verify_token.decode('utf-8'))
-#
-#     return verify_token
 
 # 이메일 인증 링크에 들어갈 토큰 생성(암호화)
 def create_verify_token(group_query,user_query):
@@ -47,6 +33,7 @@ def create_verify_token(group_query,user_query):
 
     return verify_token
 
+
 # 이메일 인증 링크에 들어있던 토큰 복호화
 def decode_verify_token(encoded_verify_token):
     decoded_token = jwt.decode(encoded_verify_token, "SECRET_KEY", algorithms=['HS256'])
@@ -56,19 +43,20 @@ def decode_verify_token(encoded_verify_token):
 
     return decoded_token['group_id']
 
+
 # 그룹 초대 링크 이메일 보내는 함수
 def send_verification_mail(group_query, participants, user_query):
-    #password = getpass.getpass('Password : ')
-    password = 'roqkfwk1234'
+    # password = getpass.getpass('Password : ')
+    password = 'real-password'
     print("password? : "+password)
-    #print('request.data',end='')
-    #print(request.data)
+    # print('request.data',end='')
+    # print(request.data)
     # 그룹 이름
     group_name = group_query.group_name
     print("send_verification_mail_group_name : " + str(group_query.group_name))
     # 그룹 참여자의 이메일
-    #participants = request.data['members']
-    print("send_verification_mail_participants : ",end='')
+    # participants = request.data['members']
+    print("send_verification_mail_participants : ", end='')
     print(participants)
 
     message = EmailMessage()
@@ -76,9 +64,8 @@ def send_verification_mail(group_query, participants, user_query):
     message['From'] = 'dpwlsl9504@naver.com'
     message['To'] = participants
 
-
-    uid = str(urlsafe_base64_encode(force_bytes(user_query.id)),'utf-8')
-    verify_token = str(urlsafe_base64_encode(force_bytes(create_verify_token(group_query,user_query))),'utf-8')
+    uid = str(urlsafe_base64_encode(force_bytes(user_query.id)), 'utf-8')
+    verify_token = str(urlsafe_base64_encode(force_bytes(create_verify_token(group_query, user_query))), 'utf-8')
 
     print(uid)
     print(verify_token)
@@ -95,78 +82,21 @@ def send_verification_mail(group_query, participants, user_query):
     # contents  = """Hello \n{}""".format(link)
 
     message.set_content(contents)
-
     # send_mail(message,contents)
 
     print('Verification Email Send')
 
     # TODO 레디스에 토큰 저장
-
     # r  = requests.post()
 
     with smtplib.SMTP_SSL('smtp.naver.com',465) as server:
         server.ehlo()
         server.login('dpwlsl9504', password)
         server.send_message(message)
-    #
-
-
-# # 그룹 초대 링크 이메일 보내는 함수
-# def send_verification_mail(groupId, participants, queryset):
-#     #password = getpass.getpass('Password : ')
-#     password = 'roqkfwk1234'
-#     print("password? : "+password)
-#     #print('request.data',end='')
-#     #print(request.data)
-#     # 그룹 이름
-#     #group_name = request.data['group_name']
-#     print("send_verification_mail_group_name : " + str(groupId))
-#     # 그룹 참여자의 이메일
-#     #participants = request.data['members']
-#     print("send_verification_mail_participants : ",end='')
-#     print(participants)
-#
-#     message = EmailMessage()
-#     message['Subject'] = 'Verification Email : Participate in your group!'
-#     message['From'] = 'dpwlsl9504@naver.com'
-#     message['To'] = participants
-#
-#
-#     uid = str(urlsafe_base64_encode(force_bytes(queryset.id)),'utf-8')
-#     verify_token = str(urlsafe_base64_encode(force_bytes(create_verify_token(groupId,queryset))),'utf-8')
-#
-#     print(uid)
-#     print(verify_token)
-#
-#     link = """{}{}/{}/""".format(const_value['PARTICIPATION_LINK'],uid,verify_token)
-#
-#     print("링크는?")
-#     print(link)
-#
-#     # contents = """Hello, We are Shev! \nYou are invited to {}.\n
-#     #            If you want to participate {}, Please click the link below.\n\n{}
-#     #            """.format(group_name,group_name,link)
-#
-#     contents  = """Hello \n{}""".format(link)
-#
-#     message.set_content(contents)
-#
-#     #send_mail(message,contents)
-#
-#     with smtplib.SMTP_SSL('smtp.naver.com',465) as server:
-#         server.ehlo()
-#         server.login('dpwlsl9504', password)
-#         server.send_message(message)
-#
-#     print('Verification Email Send')
-#
-#     # redis_set(verify_token, uid)
-
-
 
 
 def send_registration_mail(self, serializer):
-    password = 'roqkfwk1234'
+    password = 'real-password'
 
     message = EmailMessage()
     message['Subject'] = 'Confirmation Email : Welcome to Shev!'
@@ -192,10 +122,3 @@ def send_registration_mail(self, serializer):
     print('Registration Email Send')
 
 
-# def send_mail(message, password):
-#     with smtplib.SMTP_SSL('smtp.naver.com', 465) as server:
-#         server.ehlo()
-#         server.login('dpwlsl9504', password)
-#         server.send_message(message)
-#
-#     return
